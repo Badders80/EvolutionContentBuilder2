01061138bb394a131d37511207af31dd0aa00ce9
Alex Baddeley <baddeley0@gmail.com>
2025-12-03 12:10:03 +1300

    chore: adjust workspace background

--- src/components/workspace/WorkspacePanel.tsx
+++ src/components/workspace/WorkspacePanel.tsx
@@ -4,7 +4,7 @@ import { Composer } from "./Composer";
 
 export const WorkspacePanel: React.FC = () => {
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full flex-col bg-es-surfaceAlt">
       <div className="flex-1 overflow-y-auto">
         <StructuredFields />
       </div>
